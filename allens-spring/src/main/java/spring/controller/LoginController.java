0a9a1029eae4b03beefd1948f7015e3ce5bd9f97
喻越 <yuy@olymtech.com>
2020-04-27 11:26:47 +0800

    fix(controller):修复设计不合理问题

--- allens-spring/src/main/java/spring/controller/LoginController.java
+++ allens-spring/src/main/java/spring/controller/LoginController.java
@@ -1,14 +1,20 @@
 package spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.service.TestService;
 import spring.utils.Result;
 
 @RestController
 public class LoginController {
 
+    @Autowired
+    private TestService testService;
+
     @GetMapping("/test")
     public Result testReturn () {
+        System.out.println(testService.testRequest("some"));
         Result<String> result = new Result<>();
         result.setCode("0");
         result.setMsg("返回成功");
